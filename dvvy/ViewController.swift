35ddbfcb01ced5cdc489d520a9486319422296dd
David Bartholomew <dbartholomew13@apu.edu>
2018-01-28 11:30:27 -0800

    Initial Commit

---
+++ dvvy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dvvy
+//
+//  Created by David Bartholomew on 1/28/18.
+//  Copyright © 2018 David Bartholomew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
