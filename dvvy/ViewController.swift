aa67477dafe95e6f02373815df829d57788893b8
David B <eldor@Davids-iMac.local>
2018-01-28 20:09:35 -0800

    Adds comments to viewcontroller

--- dvvy/ViewController.swift
+++ dvvy/ViewController.swift
@@ -22,7 +22,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    
+    //If anywhere is touched outside the menu, it closes
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //Makes sure that the menu is showing so it doesn't toggle
+        //when it is not showing
         if (menuShowing){
             let touch = touches.first
             guard let location = touch?.location(in: self.view) else { return }
@@ -32,10 +36,13 @@ class ViewController: UIViewController {
         }
     }
     
+    //This is the function that will change the leading constraint
+    //option to allow the sidebar to open.
     func openMenu() {
         if (menuShowing) {
             leadingConstraint.constant = -150
             
+            //Creates the sliding animation
             UIView.animate(withDuration: 0.3, animations: {
                 self.view.layoutIfNeeded()
             })
@@ -43,14 +50,17 @@ class ViewController: UIViewController {
         else {
             leadingConstraint.constant = 0
             
+            //Creates the sliding animation
             UIView.animate(withDuration: 0.3, animations: {
                 self.view.layoutIfNeeded()
             })
         }
         
+        //Switches the global boolean
         menuShowing = !menuShowing
     }
 
+    //The actual outlet for the menu button
     @IBAction func openMenu(_ sender: Any) {
         openMenu()
     }
