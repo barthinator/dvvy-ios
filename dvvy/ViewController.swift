fe1cd1afe151b7bb7fe19527233a07e5e9d766e1
David Bartholomew <dbartholomew13@apu.edu>
2018-01-28 19:04:06 -0800

    Initial commit and setup

--- dvvy/ViewController.swift
+++ dvvy/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var menuView: UIView!
+    
+    var menuShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        guard let location = touch?.location(in: self.view) else { return }
+        if !menuView.frame.contains(location) {
+            openMenu()
+        }
+    }
+    
+    func openMenu() {
+        if (menuShowing) {
+            leadingConstraint.constant = -150
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        else {
+            leadingConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+        menuShowing = !menuShowing
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: Any) {
+        openMenu()
     }
-
 
 }
 
