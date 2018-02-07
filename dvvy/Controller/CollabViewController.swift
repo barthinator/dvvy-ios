0cf0c0a6bf415397d40ab5a74597195fd1329b72
Keaka Kaakau <Keakamoana@KeakaKaakau.local>
2018-02-06 20:09:50 -0800

    Keaka fixes

---
+++ dvvy/Controller/CollabViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CollabViewController.swift
+//  dvvy
+//
+//  Created by David B on 2/6/18.
+//  Copyright © 2018 David Bartholomew. All rights reserved.
+//
+
+import UIKit
+
+class CollabViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
---
+++ dvvy/Controller/MenuViewController.swift
@@ -0,0 +1,120 @@
+//
+//  MenuViewController.swift
+//  dvvy
+//
+//  Created by David B on 2/4/18.
+//  Copyright © 2018 David Bartholomew. All rights reserved.
+//
+
+import UIKit
+
+protocol SlideMenuDelegate {
+    func slideMenuItemSelectedAtIndex(_ index : Int32)
+}
+
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    /**
+     *  Array to display menu options
+     */
+    @IBOutlet var tblMenuOptions : UITableView!
+    
+    /**
+     *  Transparent button to hide menu
+     */
+    @IBOutlet var btnCloseMenuOverlay : UIButton!
+    
+    /**
+     *  Array containing menu options
+     */
+    var arrayMenuOptions = [Dictionary<String,String>]()
+    
+    /**
+     *  Menu button which was tapped to display the menu
+     */
+    var btnMenu : UIButton!
+    
+    /**
+     *  Delegate of the MenuVC
+     */
+    var delegate : SlideMenuDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tblMenuOptions.tableFooterView = UIView()
+        btnCloseMenuOverlay.alpha = 1
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateArrayMenuOptions()
+    }
+    
+    func updateArrayMenuOptions(){
+        //Pass in icon with ["title":"Name", "icon":"Name"]
+        arrayMenuOptions.append(["title":"Collab"])
+        arrayMenuOptions.append(["title":"Feed"])
+        
+        tblMenuOptions.reloadData()
+    }
+    
+    @IBAction func onCloseMenuClick(_ button:UIButton!){
+        btnMenu.tag = 0
+        
+        if (self.delegate != nil) {
+            var index = Int32(button.tag)
+            if(button == self.btnCloseMenuOverlay){
+                index = -1
+            }
+            delegate?.slideMenuItemSelectedAtIndex(index)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            self.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width,height: UIScreen.main.bounds.size.height)
+            self.view.layoutIfNeeded()
+            self.view.backgroundColor = UIColor.clear
+        }, completion: { (finished) -> Void in
+            self.view.removeFromSuperview()
+            self.removeFromParentViewController()
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cellMenu")!
+        
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        cell.layoutMargins = UIEdgeInsets.zero
+        cell.preservesSuperviewLayoutMargins = false
+        cell.backgroundColor = UIColor.clear
+        
+        let lblTitle : UILabel = cell.contentView.viewWithTag(101) as! UILabel
+        //let imgIcon : UIImageView = cell.contentView.viewWithTag(100) as! UIImageView
+        
+        //To enable icons
+        //imgIcon.image = UIImage(named: arrayMenuOptions[indexPath.row]["icon"]!)
+        lblTitle.text = arrayMenuOptions[indexPath.row]["title"]!
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let btn = UIButton(type: UIButtonType.custom)
+        btn.tag = indexPath.row
+        self.onCloseMenuClick(btn)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMenuOptions.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+}
+
