e64b3e664628384d862b6af6021ae120135c939c
David Bartholomew <dbartholomew13@apu.edu>
2018-02-06 20:55:40 -0800

    Inital profile commit

---
+++ dvvy/Controller/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  dvvy
+//
+//  Created by David Bartholomew on 2/6/18.
+//  Copyright © 2018 David Bartholomew. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Calls the slide menu button
+        addSlideMenuButton()
+    }
+    
+}
+
