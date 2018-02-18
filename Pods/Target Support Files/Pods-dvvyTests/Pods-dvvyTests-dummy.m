52b075de4c39a955185a7aace192e55e88164f22
David Bartholomew <dbartholomew13@apu.edu>
2018-02-18 01:36:37 -0800

    Adds basic authentication and login

---
+++ Pods/Target Support Files/Pods-dvvyTests/Pods-dvvyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dvvyTests : NSObject
+@end
+@implementation PodsDummy_Pods_dvvyTests
+@end
---
+++ dvvy/Controller/SignInViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SignInViewController.swift
+//  dvvy
+//
+//  Created by David Bartholomew on 2/18/18.
+//  Copyright © 2018 David Bartholomew. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignInViewController : UIViewController {
+    
+    @IBOutlet weak var signInBtn: UIButton!
+    
+    @IBOutlet weak var usernameLbl: UITextField!
+    
+    @IBOutlet weak var passwordLbl: UITextField!
+    
+    
+    @IBAction func logInPressed(_ sender: Any) {
+        // TODO: Make username actually add to database, its not actually doing anything rignt now
+        
+        // TODO: Make some sort of email / pass validation
+        if let email = usernameLbl.text, let pass = passwordLbl.text {
+            Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
+                // ...
+                if let u = user {
+                    // User found go to home screen
+                    self.performSegue(withIdentifier: "pushToHome", sender: self)
+                    print(u.email ?? "broken")
+                }
+                else {
+                    // Error: check error and message
+                    print (error ?? "nothing")
+                }
+            }
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        usernameLbl.resignFirstResponder()
+        passwordLbl.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
