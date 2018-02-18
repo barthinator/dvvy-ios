f23dc758d568e4c6c907d402367ab4555da40466
David Bartholomew <dbartholomew13@apu.edu>
2018-02-18 01:49:36 -0800

    Removes app delegate stuff

--- dvvy/AppDelegate.swift
+++ dvvy/AppDelegate.swift
@@ -18,9 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-//        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
-//
-//        }
         return true
     }
 
