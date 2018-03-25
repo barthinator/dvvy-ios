44c27f29b9a3ca7aed2c07f7fd51e8d2dec60167
Zack Goldstein <zackgoldstein@Zacks-iMac.local>
2018-03-25 00:18:46 -0700

    submit screen front end (constraint error)

--- dvvy/Controller/SubmitViewController.swift
+++ dvvy/Controller/SubmitViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class SubmitViewController : BaseViewController {
     
+    @IBOutlet var readyLbl: UILabel!
+    @IBOutlet var checkBtn: UIButton!
+    @IBOutlet var submitBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
+        
+        checkBtn.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+        checkBtn.setTitleColor(UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0), for: .normal)
+        submitBtn.setTitleColor(UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0), for: .normal)
+        
+        checkBtn.layer.cornerRadius = 5
+        submitBtn.layer.cornerRadius = 5
+        readyLbl.layer.cornerRadius = 7
         
         // Do any additional setup after loading the view.
         //readyLbl.layer.cornerRadius = 15
