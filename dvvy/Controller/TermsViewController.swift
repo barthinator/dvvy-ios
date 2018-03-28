6d1a3efcc6e2fee31a1f4e8d08176400702212ba
David Bartholomew <dbartholomew13@apu.edu>
2018-03-28 10:15:44 -0700

    fix terms page

--- dvvy/Controller/TermsViewController.swift
+++ dvvy/Controller/TermsViewController.swift
@@ -1,6 +1,6 @@
 //
 //  TermsViewController.swift
-//  
+//
 //
 //  Created by Zack Goldstein on 3/26/18.
 //
@@ -8,14 +8,14 @@
 import UIKit
 
 class TermsViewController: UIViewController {
-
+    
     @IBOutlet var agreeBtn: UIButton!
     @IBOutlet var disagreeBtn: UIButton!
     @IBOutlet var termsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         agreeBtn.layer.cornerRadius = 9
         disagreeBtn.layer.cornerRadius = 9
@@ -25,21 +25,22 @@ class TermsViewController: UIViewController {
         termsView.layer.borderWidth = 3
         termsView.layer.borderColor = UIColor.black.cgColor
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
