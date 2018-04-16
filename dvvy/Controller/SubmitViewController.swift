afbfc600490bd1694239bc5856c6e45f313496f1
jkirschenmann12 <jkrischenmann12@apu.eedu>
2018-04-15 18:51:22 -0700

    Submissions page and Terms

--- dvvy/Controller/SubmitViewController.swift
+++ dvvy/Controller/SubmitViewController.swift
@@ -11,9 +11,24 @@ import UIKit
 class SubmitViewController : BaseViewController {
     
     
+    @IBOutlet var btnCheck: UIButton!
+    @IBOutlet var btnSubmit: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
+        
+        super.viewDidLoad()
+        //Calls the slide menu button
+        addSlideMenuButton()
+        btnSubmit.layer.borderWidth = 1
+        btnSubmit.layer.cornerRadius = 5
+        btnSubmit.layer.borderColor = UIColor(red:1.00, green:0.46, blue:0.37, alpha:1.0).cgColor
+        btnCheck.layer.borderWidth = 1
+        btnCheck.layer.cornerRadius = 5
+        //btnCheck.layer.borderColor = UIColor(red:1.00, green:0.46, blue:0.37, alpha:1.0).cgColor
+        
+        
+        
         
         // Do any additional setup after loading the view.
         //readyLbl.layer.cornerRadius = 15
---
+++ dvvy/Controller/TermsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TermsViewController.swift
+//  dvvy
+//
+//  Created by Jason Kirschenmann on 4/15/18.
+//  Copyright © 2018 David Bartholomew. All rights reserved.
+//
+
+import UIKit
+
+
+class TermsViewController: BaseViewController {
+    @IBOutlet var txtView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        
+        super.viewDidLoad()
+        //Calls the slide menu button
+        addSlideMenuButton()
+        txtView.layer.borderWidth = 2
+        txtView.layer.cornerRadius = 5
+        txtView.layer.borderColor = UIColor(red:1.00, green:0.46, blue:0.37, alpha:1.0).cgColor
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
